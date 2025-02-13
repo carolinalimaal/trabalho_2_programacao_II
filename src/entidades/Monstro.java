dcff1990875653d586e51b29e6f264c395507bc0
David Galhego <129506782+DavidGBUF@users.noreply.github.com>
2025-02-13 12:40:50 -0300

    Delete src directory

--- src/entidades/Monstro.java
+++
@@ -1,9 +0,0 @@
-package entidades;
-
-public abstract class Monstro extends Jogador{
-
-	public Monstro(String nome, int hp, int ataque, int defesa, int destreza, int velocidade) {
-		super(nome, hp, ataque, defesa, destreza, velocidade);
-	}
-
-}
--- src/enums/StatusJogador.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum StatusJogador {
-	NORMAL, QUEIMANDO, ENVENENADO, VOANDO;
-}
